39eeb5e4a9ed0790855d1b8705d02d641f501dd1
tomoakimiura <tomomiura0026@gmail.com>
2021-03-01 22:20:23 +0900

    それぞれのUseCaseの修正

--- viperTest/GetArticleByIdUseCase.swift
+++ viperTest/GetArticleByIdUseCase.swift
@@ -8,6 +8,12 @@
 
 import Foundation
 
-class GetArticlesByIdUseCase {
+
+class GetArticleByIdUseCase: UseCaseProtocol {
     
+    func execute(_ parameter: Int, completion: ((Result<ArticleEntity, Error>) -> ())?) {
+        let res = ArticleEntity(id: 1, userId: 1, title: "タイトル", body: "本文")
+        
+        completion?(.success(res))
+    }
 }
--- viperTest/GetArticlesArrayUseCase.swift
+++ viperTest/GetArticlesArrayUseCase.swift
@@ -8,6 +8,14 @@
 
 import Foundation
 
-class GetArticlesArrayUseCase {
+class GetArticlesArrayUseCase: UseCaseProtocol {
     
+    func execute(_ parameter: Void, completion: ((Result<[ArticleEntity], Error>) -> ())?) {
+        let res: [ArticleEntity] = [
+            ArticleEntity(id: 1, userId: 1, title: "タイトル", body: "本文"),
+            ArticleEntity(id: 2, userId: 1, title: "タイトル2", body: "本文2"),
+            ArticleEntity(id: 3, userId: 1, title: "タイトル3", body: "本文3"),
+        ]
+        completion?(.success(res))
+    }
 }
